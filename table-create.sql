e4d0b74e3fd4ea513f5a8edec7abe3c9fef41f49
lidetian01 <lidetian_qd@caxins.com>
2018-11-06 22:28:41 +0800

    the sql of creating table

---
+++ table-create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `dept` (
+  `deptno` bigint(20) NOT NULL AUTO_INCREMENT,
+  `dname` varchar(50) DEFAULT NULL,
+  `loc` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`deptno`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8
